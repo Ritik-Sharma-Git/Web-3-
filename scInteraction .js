5948b3ecde4165ef75cdebdef8081f0192c8408e
Ritik-Sharma-Git <99575617+Ritik-Sharma-Git@users.noreply.github.com>
2025-07-23 23:15:41 +0530

    your update message here

--- scInteraction .js
+++ scInteraction .js
@@ -2,10 +2,10 @@ const ethers= require("ethers");
 const abi = require("./abi.json");
 
 async function readContract() {
-   const provider = new ethers.JsonRpcProvider("https://ethereum.publicnode.com");
-   const contractAddress = "0xE6889ae4600113cb058b1bCd2152Efa1c4CFFe27";
+   const provider = new ethers.JsonRpcProvider("https://ethereum-sepolia-rpc.publicnode.com");
+   const contractAddress = "0x5497123F78BD3543e1c5807Aa4A292caDA2d58c6";
    const contractInstance = new ethers.Contract(contractAddress, abi, provider);
-   const value = contractInstance.retrieve();
+   const value =  await contractInstance.retrieve();
    console.log(value);
 }
 
